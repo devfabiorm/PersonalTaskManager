5cbfe30c064c1966c708aebd7e64d5ec4e792700
Fabio Ribeiro Martins <f.ribeiro.martins@avanade.com>
2024-03-28 13:34:22 -0300

    including log

--- PersonalTaskManager.Application/Commands/CreateTask/CreateTaskCommandHandler.cs
+++ PersonalTaskManager.Application/Commands/CreateTask/CreateTaskCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using PersonalTaskManager.Core.Models;
+﻿using Microsoft.Extensions.Logging;
+using PersonalTaskManager.Core.Models;
 using PersonalTaskManager.Infrastructure.Persistence.Repository.Implementation;
 using System.Threading.Tasks;
 
@@ -7,16 +8,19 @@ namespace PersonalTaskManager.Application.Commands.CreateTask
     public class CreateTaskCommandHandler
     {
         private readonly TaskRepository _taskRepository;
+        private readonly ILogger _logger;
 
         public CreateTaskCommandHandler()
         {
             _taskRepository = new TaskRepository();
+            _logger = new LoggerFactory().CreateLogger<CreateTaskCommandHandler>();
         }
 
         public async Task<int> Handle(CreateTaskCommand request)
         {
             var task = new PersonalTask(request.Title, request.LimitDate, request.CategoryId, request.Details);
 
+            _logger.LogInformation("Saving data");
             await _taskRepository.CreateAsync(task);
 
             return task.Id;
